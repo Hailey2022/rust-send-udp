37495a25ac3d96b6f4a2a654c9e11604b0069e45
cc <cc-github@gmail.com>
2022-11-05 18:32:09 +0800

    faster but not fast enough

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,22 @@
-use std::net::UdpSocket;
+use smol::net::UdpSocket;
 
-fn send(ip: &str, port: u16) -> anyhow::Result<()> {
+async fn send(ip: &str, port: u16) -> anyhow::Result<()> {
+    let bind_addr = format!("0.0.0.0:{}", port);
     let addr = format!("{}:{}", ip, port);
-    let socket = UdpSocket::bind("0.0.0.0:8888")?;
-    socket.send_to(b"hello", addr)?;
+    let socket = UdpSocket::bind(bind_addr).await?;
+    socket.send_to(b"hello", addr).await?;
     Ok(())
 }
 
 fn main() {
     let mut port = 1u16;
     loop {
-        if let Err(e) = send("114.114.114.114", port) {
-            eprintln!("{e}");
-        }
+        smolscale::spawn(async move {
+            send("114.114.114.114", port).await
+        }).detach();
         if port < 65535 {
             port += 1;
-        }else{
+        } else {
             port = 1;
         }
     }
