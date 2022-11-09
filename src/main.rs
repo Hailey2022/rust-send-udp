d79702715bde2393b9e4d3d87b55670ccd7be7e1
Hailey2022 <96413516+Hailey2022@users.noreply.github.com>
2022-11-10 02:14:26 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,20 @@
-// use std::io::{BufRead, Write};
 use std::net::UdpSocket;
+use std::collections::hash_map::RandomState;
+use std::hash::{BuildHasher, Hasher};
 
-async fn send(ip: &str) -> anyhow::Result<()> {
+async fn send() -> anyhow::Result<()> {
     let bind_addr = format!("0.0.0.0:0");
     let socket = UdpSocket::bind(bind_addr)?;
-    for port in 1..65535u16 {
-        let addr = format!("{}:{}", ip, port);
-        socket.send_to(b"hello", addr)?;
-    }
+    let tmp = RandomState::new().build_hasher().finish().to_ne_bytes();
+    let addr = format!("{}.{}.{}.{}:{}", tmp[0], tmp[1], tmp[2], tmp[3], tmp[4]);
+    socket.send_to(b"hello", addr)?;
     Ok(())
 }
 
-// fn prompt(message: &str) -> anyhow::Result<String> {
-//     let stdout = std::io::stdout();
-//     let mut stdout = stdout.lock();
-//     stdout.write_all(message.as_bytes())?;
-//     stdout.flush()?;
-//     let stdin = std::io::stdin();
-//     let mut stdin = stdin.lock();
-//     let mut line = String::new();
-//     stdin.read_line(&mut line)?;
-//     Ok(line)
-// }
-
 fn main() {
-//     let ip = match prompt("ip:") {
-//         Ok(ip) => {
-//             println!("using {ip}");
-//             ip
-//         },
-//         Err(e) => {
-//             println!("using 1.1.1.1 since {e}");
-//             String::from("1.1.1.1")
-//         }
-//     };
-//     let mut port = 1u16;
     loop {
         smolscale::spawn(async move {
-            send("114.114.114.114").await
+            send().await
         }).detach();
-//         if port < 65535 {
-//             port += 1;
-//         } else {
-//             println!("again!");
-//             port = 1;
-//         }
     }
 }
